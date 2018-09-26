6640a4bf00d02d061b620deb4851fb812f185ed2
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2018-09-26 13:49:26 +0300

    Manifest and url

--- 02/ResNext/setup.py
+++ 02/ResNext/setup.py
@@ -6,5 +6,6 @@ setup(
     packages=['resnext'],
     license='MIT license',
     long_description=open('README.txt').read(),
+    url='http://pypi.python.org/pypi/ResNext/',
     author='Egor Bogomolov'
 )
